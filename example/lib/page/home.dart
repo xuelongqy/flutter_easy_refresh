7a61611f5bb34bd56925ec1743c3165a531ad932
knoyo <59814509@qq.com>
2020-01-30 18:09:48 +0800

    修改：优化主页面

--- example/lib/page/home.dart
+++ example/lib/page/home.dart
@@ -10,37 +10,45 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage> {
   // 页面控制
-  TabController _tabController;
+  PageController _pageController;
+  // 当前页面
+  int _pageIndex = 0;
 
   // 初始化
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
-    _tabController.addListener(() {
-      setState(() {});
-    });
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   // 底部栏切换
   void _onBottomNavigationBarTap(int index) {
-    setState(() {
-      _tabController.index = index;
-    });
+    _pageController.animateToPage(index,
+        duration: Duration(milliseconds: 200), curve: Curves.linear);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TabBarView(
-        controller: _tabController,
+      body: PageView(
+        controller: _pageController,
         children: <Widget>[SamplePage(), StylePage(), MorePage()],
+        onPageChanged: (index) {
+          setState(() {
+            _pageIndex = index;
+          });
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _tabController.index,
+        currentIndex: _pageIndex,
         type: BottomNavigationBarType.fixed,
         fixedColor: Colors.black,
         onTap: _onBottomNavigationBarTap,
