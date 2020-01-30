5394b022d3538db597fa545b9728844f5b6c620d
knoyo <59814509@qq.com>
2020-01-30 20:07:36 +0800

    修改：优化Android启动页

--- example/android/app/src/main/kotlin/com/qingyi/easyrefresh/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/qingyi/easyrefresh/example/MainActivity.kt
@@ -2,6 +2,7 @@ package com.qingyi.easyrefresh.example
 
 import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -19,6 +20,8 @@ class MainActivity: FlutterActivity() {
   }
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    // 取消全屏
+    window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
       //API>21,设置状态栏颜色透明
       window.statusBarColor = 0
