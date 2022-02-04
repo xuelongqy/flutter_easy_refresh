6d0e25aa59b0e66ce0f2e730ad0b3e56a1e73657
xuelongqy <xuelongqy@qq.com>
2022-02-04 20:44:01 +0800

    Add: PointerDeviceKind.mouse. Work on web and pc.

--- lib/easy_refresh.dart
+++ lib/easy_refresh.dart
@@ -4,6 +4,7 @@ import 'dart:math' as math;
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/physics.dart';
 import 'package:flutter/rendering.dart';
--- lib/src/behavior/scroll_behavior.dart
+++ lib/src/behavior/scroll_behavior.dart
@@ -3,12 +3,20 @@ part of easyrefresh;
 /// 滚动行为
 /// 用于覆盖flutter自带平台相关的[ScrollBehavior]
 class ERScrollBehavior extends ScrollBehavior {
-  final ScrollPhysics _physics;
+  final ERScrollPhysics _physics;
 
-  ERScrollBehavior(this._physics);
+  const ERScrollBehavior(this._physics);
 
   @override
-  ScrollPhysics getScrollPhysics(BuildContext context) {
+  ERScrollPhysics getScrollPhysics(BuildContext context) {
     return _physics;
   }
+
+  @override
+  Set<PointerDeviceKind> get dragDevices => <PointerDeviceKind>{
+    PointerDeviceKind.touch,
+    PointerDeviceKind.stylus,
+    PointerDeviceKind.invertedStylus,
+    PointerDeviceKind.mouse,
+  };
 }
