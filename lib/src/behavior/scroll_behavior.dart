bf5fa0c26b7237afcd24cc8a54d46eabb84f1d33
xuelong <xuelongqy@qq.com>
2023-08-23 14:47:13 +0800

    chore: Update dependencies.

--- lib/src/behavior/scroll_behavior.dart
+++ lib/src/behavior/scroll_behavior.dart
@@ -3,6 +3,9 @@ part of easy_refresh;
 /// Define [ScrollBehavior] in the scope of EasyRefresh.
 /// Add support for web and PC.
 class ERScrollBehavior extends ScrollBehavior {
+  static final Set<PointerDeviceKind> _kDragDevices =
+      PointerDeviceKind.values.toSet();
+
   final ScrollPhysics? _physics;
 
   const ERScrollBehavior([this._physics]);
@@ -19,11 +22,5 @@ class ERScrollBehavior extends ScrollBehavior {
   }
 
   @override
-  Set<PointerDeviceKind> get dragDevices => <PointerDeviceKind>{
-        PointerDeviceKind.touch,
-        PointerDeviceKind.stylus,
-        PointerDeviceKind.invertedStylus,
-        PointerDeviceKind.mouse,
-        PointerDeviceKind.unknown,
-      };
+  Set<PointerDeviceKind> get dragDevices => _kDragDevices;
 }
